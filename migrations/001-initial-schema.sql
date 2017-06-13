1dadc66dc5769ce0e06ed121b41737ed4cacbae8
Mike Grill <mike@gogrillion.com>
2017-06-12 21:06:38 -0400

    init commit

---
+++ migrations/001-initial-schema.sql
@@ -0,0 +1,14 @@
+-- Up
+CREATE TABLE files
+(
+  id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+  path TEXT NOT NULL,
+  size UNSIGNED BIG INT NOT NULL,
+  md5 VARCHAR(32)
+);
+CREATE UNIQUE INDEX files_id_uindex ON files (id);
+CREATE UNIQUE INDEX files_path_uindex ON files (path);
+CREATE INDEX md5_index ON files (md5);
+
+-- Down
+DROP TABLE files;
---
+++ resources/samples/klaw-scan-item.js
@@ -0,0 +1,54 @@
+/**
+ * @typedef {Object} KlawScanStats
+ * @property {Number} dev
+ * @property {Number} mode
+ * @property {Number} nlink
+ * @property {Number} uid
+ * @property {Number} gid
+ * @property {Number} rdev
+ * @property {Number} blksize
+ * @property {Number} ino
+ * @property {Number} size
+ * @property {Number} blocks
+ * @property {Number} atimeMs
+ * @property {Number} mtimeMs
+ * @property {Number} ctimeMs
+ * @property {Number} birthtimeMs
+ * @property {String} atime
+ * @property {String} mtime
+ * @property {String} ctime
+ * @property {String} birthtime
+ */
+
+/**
+ * @typedef {Object} KlawScanItem
+ * @property {String} path
+ * @property {KlawScanStats} stats
+ */
+
+/**
+ * @type {KlawScanItem} item
+ */
+var item = {
+  "path": "/Users/mgrill/Workspace/snes9x/snes9x/macosx/mac-client.cpp",
+  "stats": {
+    "dev": 16777220,
+    "mode": 33188,
+    "nlink": 1,
+    "uid": 501,
+    "gid": 20,
+    "rdev": 0,
+    "blksize": 4096,
+    "ino": 1397100,
+    "size": 37650,
+    "blocks": 80,
+    "atimeMs": 1488520690000,
+    "mtimeMs": 1488512051000,
+    "ctimeMs": 1488512051000,
+    "birthtimeMs": 1488512051000,
+    "atime": "2017-03-03T05:58:10.000Z",
+    "mtime": "2017-03-03T03:34:11.000Z",
+    "ctime": "2017-03-03T03:34:11.000Z",
+    "birthtime": "2017-03-03T03:34:11.000Z"
+  }
+};
